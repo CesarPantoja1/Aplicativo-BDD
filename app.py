d515c2bf0f1afef9ae05faeeae14b66349645d9e
Sebasjasan <sebastiansanchez200311@gmail.com>
2025-01-22 18:28:37 -0600

    Pantallas html, css y js terminadas

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")  
 
-
-
 @app.route("/clientesinfo")
-def clientes():
+def clientesInfo():
     return render_template("clientesinfo.html")  
+
+@app.route("/clientesLaboral")
+def clientesLaboral():
+    return render_template("clientesMembresia.html") 
 
 @app.route("/register")
 def register():
@@ -19,6 +21,30 @@ def register():
 @app.route("/ingreso")
 def ingreso():
     return render_template("ingreso.html")  
+
+@app.route("/empleadosInfo")
+def empleadoInfo():
+    return render_template("empleadosInfo.html")  
+
+@app.route("/empleadosLaboral")
+def empleadoLaboral():
+    return render_template("empleadosLaboral.html")  
+
+@app.route("/producto")
+def producto():
+    return render_template("producto.html")  
+
+@app.route("/proveedor")
+def proveedor():
+    return render_template("proveedor.html")  
+
+@app.route("/factura")
+def factura():
+    return render_template("factura.html")  
+
+@app.route("/detalleFactura")
+def detalleFactura():
+    return render_template("detalleFactura.html") 
 
 if __name__ == "__main__":
     app.run(debug=True)
