b2e0c790d71f4acc60450ffea6eff9e7950222d9
CESAR HECTOR PANTOJA ARMAS <cesar.pantoja01@epn.edu.ec>
2025-02-06 01:31:37 -0500

    Update Proveedores remoto

--- app.py
+++ app.py
@@ -555,30 +555,24 @@ def update_producto():
     
 @app.route('/proveedor', methods=['PUT'])
 def update_proveedor():
-    tienda = session.get("tienda")
-    if not tienda:
-        return jsonify({"error": "No se ha seleccionado una tienda"}), 400
-    
-    data = request.get_json()
+    data = request.get_json()  
     proveedor_id = data.get("proveedorID")
-    tienda_id = data.get("tiendaID")
-    if not proveedor_id or not tienda_id:
-        return jsonify({"error": "Falta proveedorID o tiendaID"}), 400
-    
+    tienda_id = data.get("tiendaID")  
+
     proveedor = Proveedor.query.filter_by(proveedorID=proveedor_id, tiendaID=tienda_id).first()
-    if not proveedor:
-        return jsonify({"error": "Proveedor no encontrado"}), 404
-    
     try:
+        # Actualizar solo si los valores están en la solicitud
         proveedor.nombreProveedor = data.get("nombreProveedor", proveedor.nombreProveedor)
         proveedor.ciudad = data.get("ciudad", proveedor.ciudad)
-        proveedor.telefono = data.get("telefono", proveedor.telefono)    
-        dbQuito.session.commit()
+        proveedor.telefono = data.get("telefono", proveedor.telefono)
+
+        dbQuito.session.commit()  # Guardar cambios en la base de datos
         return jsonify({"message": "Proveedor actualizado correctamente"}), 200
-    
+
     except Exception as e:
         dbQuito.session.rollback()
         return jsonify({"error": str(e)}), 500
+
 
 
 @app.route('/empleadoInfo', methods=['PUT'])
